c740a77c398a7040b71ae45c30810dfa44f4c54c
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2023-11-10 12:14:29 +0900

    add animation progress bar, fix value js

--- script.js
+++ script.js
@@ -9,13 +9,15 @@ btn.addEventListener("click", ()=> {
     const statusInputValue = statusInput.value;
     const inputValue = input.value;
     const title = document.querySelector(".title")
-
-    if (!isNaN(inputValue) && !isNaN(statusInputValue)) {
+    if ((statusInputValue * 100 / inputValue) > 100 ) {
+        alert("実際解いた問数は必ず目標問数より小さい！！")
+    } else if (!isNaN(inputValue) && !isNaN(statusInputValue)) {
         title.textContent = "今日のあなたの目標は" + inputValue + "問";
         status.textContent = statusInputValue + "問" 
         active.style.width = statusInputValue * 100 / inputValue + "%"
     }else {
         alert("数値だけに入力してください！")
     }
+    
     
 })
